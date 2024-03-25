b5f00860ec5d45b52b4cd0f21570608d58b76758
WANG <wangqianlf@live.com>
2024-03-25 22:36:28 +0100

    méthode ajout une entrée dans la listView avec le bouton ajouter est fait, mais le chargement de démarrage de la liste n'est pas configuré

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -135,6 +135,7 @@
             this.button1.TabIndex = 7;
             this.button1.Text = "Ajouter";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
--- Form1.cs
+++ Form1.cs
@@ -17,5 +17,37 @@ namespace Gestion_de_contacts
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Contacts c = new Contacts(textBox1.Text, textBox2.Text, textBox3.Text);
+            Contacts.SaveContact(c);
+            RefreshListView();
+
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+
+        //Mérhode qui permet d'affichier le contact ajouté dans la listView
+        void RefreshListView()
+        {
+            listView1.Items.Clear();
+            //Ici on utilise directement la class, et on n'a pas besoin d'instancier un objet,
+            // car la méthode (ListContacts) est static
+
+            Contacts.LoadFromFile();
+
+            //Rafraichir la listView avec une boucle foreach
+            foreach (Contacts item in Contacts.ListContacts)
+            {
+                ListViewItem lvi = new ListViewItem();
+                lvi.Text = item.Nom;
+                lvi.SubItems.Add(item.Prenom);
+                lvi.SubItems.Add(item.Telephone);
+                //ajouter tous ces élements dans la listView
+                listView1.Items.Add(lvi);
+            }
+        }
     }
 }
